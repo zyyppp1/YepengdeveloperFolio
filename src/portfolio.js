bed25060d4f0b669ee498c27eceeebd02ba710e3
Joey-Yepeng <joey.yepeng@gmail.com>
2025-11-10 13:40:25 +0000

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -269,7 +269,7 @@ const achievementSection = {
       title: "The Complete Full-Stack Web Development Bootcamp",
       subtitle:
         "Udemy · Issued Oct 2025 · Credential ID UC-04d9d0f3-4d7b-49c1-a681-1cb0a49830d9",
-      image: require("./assets/images/Udemy Logo.svg"),
+      image: require("./assets/images/UdemyLogo.svg"),
       imageAlt: "Full-Stack Web Development Bootcamp Certificate",
       footerLink: [
         {
@@ -282,7 +282,7 @@ const achievementSection = {
       title: "Python Data Structures & Algorithms + LEETCODE Exercises",
       subtitle:
         "Udemy · Credential ID UC-3fee7427-f354-4a7d-b6ac-3bc81d0e4dd9",
-      image: require("./assets/images/Udemy Logo.svg"),
+      image: require("./assets/images/UdemyLogo.png"),
       imageAlt: "Python Data Structures & Algorithms Certificate",
       footerLink: [
         {
