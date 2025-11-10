637b4303aa4a8636a47f78aecd10de7d8aee1e7d
Joey-Yepeng <joey.yepeng@gmail.com>
2025-11-10 12:26:59 +0000

    Change showGithubProfile to false

--- src/portfolio.js
+++ src/portfolio.js
@@ -206,7 +206,7 @@ const workExperiences = {
 To know how to get github key look at readme.md */
 
 const openSource = {
-  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
+  showGithubProfile: "false", // Set true or false to show Contact profile using Github, defaults to true
   display: false // Set false to hide this section, defaults to true
 };
 
